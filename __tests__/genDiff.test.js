1ad4b1bb199ff8c97dcb0ea283ce853a709e1d2a
EvaBoxler <e.a.boksler@gmail.com>
2022-07-12 13:36:35 +0700

    test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -7,15 +7,29 @@ const expectedPlainOutput = readFile('plainOutput.txt');
 const expectedJsonOutput = readFile('jsonOutput.txt');
 
 test.each([
-  { file1: 'file1.json', file2: 'file2.json', expected: expectedStylishOutput, format: 'stylish', description: 'stylish format - json files' },
-  { file1: 'file1.yml', file2: 'file2.yaml', expected: expectedStylishOutput, format: 'stylish', description: 'stylish format - yaml files' },
+  {
+    file1: 'file1.json', file2: 'file2.json', expected: expectedStylishOutput, format: 'stylish', description: 'stylish format - json files',
+  },
+  {
+    file1: 'file1.yml', file2: 'file2.yaml', expected: expectedStylishOutput, format: 'stylish', description: 'stylish format - yaml files',
+  },
 
-  { file1: 'file1.json', file2: 'file2.json', expected: expectedPlainOutput, format: 'plain', description: 'plain format - json files' },
-  { file1: 'file1.yml', file2: 'file2.yaml', expected: expectedPlainOutput, format: 'plain', description: 'plain format - yaml files' },
+  {
+    file1: 'file1.json', file2: 'file2.json', expected: expectedPlainOutput, format: 'plain', description: 'plain format - json files',
+  },
+  {
+    file1: 'file1.yml', file2: 'file2.yaml', expected: expectedPlainOutput, format: 'plain', description: 'plain format - yaml files',
+  },
 
-  { file1: 'file1.json', file2: 'file2.json', expected: expectedJsonOutput, format: 'json', description: 'json format - json files' },
-  { file1: 'file1.yml', file2: 'file2.yaml', expected: expectedJsonOutput, format: 'json', description: 'json format - yaml files' },
+  {
+    file1: 'file1.json', file2: 'file2.json', expected: expectedJsonOutput, format: 'json', description: 'json format - json files',
+  },
+  {
+    file1: 'file1.yml', file2: 'file2.yaml', expected: expectedJsonOutput, format: 'json', description: 'json format - yaml files',
+  },
 
-])('$description', ({ file1, file2, expected, format }) => {
+])('$description', ({
+  file1, file2, expected, format,
+}) => {
   expect(genDiff(file1, file2, format)).toEqual(expected);
 });
